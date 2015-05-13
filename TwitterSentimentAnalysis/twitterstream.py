63a170725833661ef2dfdf6c80313dc0fb845856
rraymondd <ramtinj1986@gmail.com>
2015-05-13 10:05:58 -0700

    Fourth commit

--- TwitterSentimentAnalysis/twitterstream.py
+++ TwitterSentimentAnalysis/twitterstream.py
@@ -3,6 +3,7 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 from pymongo import MongoClient
 import json
+import re
 
 access_token = "3166078670-kUTXvUAHyaddLaoWiibqT4aSYKhLAS2zy1I7pos"
 access_token_secret = "ZcOHVTe4PwdDCmjAdmwGfepyJWI6eXpRouCwHVvaPyFLi"
@@ -27,23 +28,25 @@ class StdOutListener(StreamListener):
         client = MongoClient()
         db = client.tweetdb
         datajson = json.loads(data)
-        trimmed={}
-        trimmed['id'] =  datajson['id']
-        trimmed['created_at'] = datajson['created_at']
-        trimmed['text'] = datajson['text']
-        trimmed['location'] = datajson['user']['location']
-        trimmed['time_zone'] = datajson['user']['time_zone']
+        
+        x =  datajson['text']
+        munged={}
+        munged['id'] =  datajson['id']
+        munged['created_at'] = datajson['created_at']
+        munged['text'] = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",x).split())
+        munged['location'] = datajson['user']['location']
+        munged['time_zone'] = datajson['user']['time_zone']
         
         
-        json_data = json.dumps(trimmed)
+        json_data = json.dumps(munged)
         db.iphonetweets.insert(json.loads(json_data))
         
         print json_data
         with open('tweets.txt', 'a') as tf:
-            tf.write(data)
+            tf.write(json_data)
         
         self.num_of_tweets = self.num_of_tweets + 1
-        if self.num_of_tweets <1:
+        if self.num_of_tweets <5:
             return True
         else:
             tf.close()
